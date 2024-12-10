3f92cbece592e80733a20a93d02a8f019dd2eba8
Caique.Braga <caiquebsousa@outlook.com>
2024-12-10 05:03:34 -0300

    FEAT - CONSEITOS JAVA - IF

---
+++ ConceitosJava/src/controleDeFluxo/IfExample.java
@@ -0,0 +1,13 @@
+package controleDeFluxo;
+
+public class IfExample {
+
+    public static void main(String[] args) {
+        int idade = 16;
+
+        if (idade < 18) {
+            System.out.println("com essa Idade de: " + idade + " anos," +  " Não é permitido dirigir");
+        }
+    }
+
+}
