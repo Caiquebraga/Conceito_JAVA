a16099254623df8b81e3d906b1c1f41cd093ba47
Caique.Braga <caiquebsousa@outlook.com>
2024-12-11 04:51:38 -0300

    [REFS - 0001 - FEAT - Trabalhando com IF_ELSE]

---
+++ ConceitosJava/src/controleDeFluxo/IfElse_Exemple.java
@@ -0,0 +1,21 @@
+package controleDeFluxo;
+import java.util.Scanner;
+
+public class IfElse_Exemple {
+
+    public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade:");
+     int idade = scanner.nextInt();
+
+     if(idade < 18){
+         System.out.println("com essa Idade de: " + idade + " anos," +  " Não é permitido dirigir");
+     } else {
+         System.out.println("com essa Idade de: " + idade + " anos," + "é permitido dirigir");
+     }
+
+     scanner.close();
+    }
+
+}
