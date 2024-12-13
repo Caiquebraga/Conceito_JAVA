c201d7708877b91b0538cae27e92c24265e9212a
Caique.Braga <caiquebsousa@outlook.com>
2024-12-13 04:31:58 -0300

    [REFS - 0003 - FEAT - Criando classe FOR]

---
+++ ConceitosJava/src/controleDeFluxo/for_Exemple.java
@@ -0,0 +1,12 @@
+package controleDeFluxo;
+
+public class for_Exemple {
+
+
+    public static void main (String[] args){
+
+        for(int i = 1; i <= 8; i++ ){
+            System.out.println(i);
+        }
+    }
+}
