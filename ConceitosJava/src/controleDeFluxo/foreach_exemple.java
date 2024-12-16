c72df32a36bc47a64e00955ad6306c877ef87640
Caique.Braga <caiquebsousa@outlook.com>
2024-12-16 04:36:35 -0300

    [REFS: 0006 - FEAT - Criação de fluxo ForEach]

---
+++ ConceitosJava/src/controleDeFluxo/foreach_exemple.java
@@ -0,0 +1,14 @@
+package controleDeFluxo;
+
+public class foreach_exemple {
+
+    public static void main(String[] args){
+
+        String[] nomes =  {"caique", "Paula", "Henri", "Davi", "Aurora"};
+
+        for(String nome : nomes){
+            System.out.println("Nome: " + nome);
+        }
+
+    }
+}
