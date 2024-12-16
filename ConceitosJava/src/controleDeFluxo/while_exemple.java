7fce281d62c0c0e376636193103f6c291f0d9431
Caique.Braga <caiquebsousa@outlook.com>
2024-12-16 04:21:29 -0300

    [REFS: 0004 - FEAT - Criação de fluxo while]

---
+++ ConceitosJava/src/controleDeFluxo/while_exemple.java
@@ -0,0 +1,15 @@
+package controleDeFluxo;
+
+public class while_exemple {
+
+    public static void main(String[] args){
+
+        int idade = 15;
+
+        while (idade < 18){
+
+            System.out.println("Sem liberação para dirigir");
+            idade++;
+        }
+    }
+}
