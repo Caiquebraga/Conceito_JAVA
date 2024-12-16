05ebe44a39beeddb6145d7ae0f829fb6a6395a15
Caique.Braga <caiquebsousa@outlook.com>
2024-12-16 04:27:53 -0300

    [REFS: 0005 - FEAT - Criação de fluxo do_while]

---
+++ ConceitosJava/src/controleDeFluxo/do_while_exemple.java
@@ -0,0 +1,15 @@
+package controleDeFluxo;
+
+public class do_while_exemple {
+
+    public static void main(String[] args){
+
+        int idade = 62;
+
+        do{
+            System.out.println("Sem idddadde para se aposentar");
+            idade++;
+        } while (idade < 65);
+            System.out.println("Agora você tem direito a aposentadoria");
+    }
+}
