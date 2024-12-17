4c8222b7992816dcd06a987e9e8e6210b721691e
Caique.Braga <caiquebsousa@outlook.com>
2024-12-17 04:24:41 -0300

    [REFS: 0007 - FEAT - Tipos e ddados - Primitivos]

---
+++ ConceitosJava/src/Tipos_de_dados/Tipos_Primitivos.java
@@ -0,0 +1,25 @@
+package Tipos_de_dados;
+
+public class Tipos_Primitivos {
+
+    public static void main(String[] args){
+        byte meuByte = 127;
+        short meuShort = 32000;
+        int meuInt = 2147483647;
+        long meuLong = 9223372036854775807L;
+
+        //TIPO FLOAT
+
+        float meuFloat = 3.14f;
+        double meuDouble = 3.141592653589793;
+
+        // Tipo Caracter
+        char meuChar = 'A';
+
+        //Tipo Booleano
+        boolean meuBolean = true;
+
+
+
+    }
+}
