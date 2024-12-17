9af6bef251d455f1e973b645bb6df3cbb460c9c0
Caique.Braga <caiquebsousa@outlook.com>
2024-12-17 04:42:02 -0300

    [REFS: 0008 - FEAT - Tipos e ddados - Referência]

---
+++ ConceitosJava/src/Tipos_de_dados/Tipos_Referencia.java
@@ -0,0 +1,12 @@
+package Tipos_de_dados;
+
+public class Tipos_Referencia {
+
+    public static void main (String[] args){
+
+        String nome = "Caique";
+
+        System.out.println("Nome " + nome) ;
+    }
+
+}
