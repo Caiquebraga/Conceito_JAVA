a7860784e5c028aa9598ac471470a4b42bb02215
Caique.Braga <caiquebsousa@outlook.com>
2024-12-30 08:15:10 -0300

    [REFS: 0015 - FEAT - Orientação a objetos - Modificadores de Acesso -PRIVATE]

---
+++ ConceitosJava/src/Orientação_A_Objetos/Modificadores_de_Acesso/private_Exemple.java
@@ -0,0 +1,14 @@
+package Orientação_A_Objetos.Modificadores_de_Acesso;
+
+public class private_Exemple {
+
+    private String Name;
+
+    public void setNome(String Name){
+        this.Name = Name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+}
